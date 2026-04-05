441c35339ecc015088572a6aeeda8122e119d6b4
mb_boris <mbboris12@gmail.com>
2026-04-05 16:25:47 +0300

    Added solution to task-4

--- js/task-4.js
+++ js/task-4.js
@@ -1,1 +1,27 @@
+const form = document.querySelector(".login-form");
 
+form.addEventListener("submit", function (event) {
+event.preventDefault();
+
+const form = document.querySelector(".login-form");
+
+form.addEventListener("submit", function (event) {
+event.preventDefault();
+    
+if (emailValue === "" || passwordValue === "") {
+    alert("All form fields must be filled in");
+    return;
+}  
+const formData = {
+    email: emailValue,
+    password: passwordValue,
+};
+
+    console.log(formData);
+
+const formData = {
+    email: emailValue,
+    password: passwordValue,
+};
+
+console.log(formData);
